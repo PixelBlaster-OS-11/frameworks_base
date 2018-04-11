fb2c11dbb06ac034e3a67380a57c1fdc983ae1c4
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    neobuddy89: Fixes and simplify.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5715,6 +5715,12 @@ public final class Settings {
         public static final String CLEAR_ALL_ICON_STYLE = "clear_all_icon_style";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
