3073147369a6d61888b7b608ca9b64353ff589da
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-04-27 18:00:52 +0200

    Battery options cleanup [1/2]
    previously there was some logic in the Settings part that was setting text percentage style
    even for the hidden style option to load the needed values in BatteryMeterView.
    That made the code really hard to read and understand, and needed an additional system setting value.
    Now we move the proper logic to BatteryMeterView so there's no confusion and possible glitches due to settings preferences loading,
    This commit also fixes at least one glitch: while charging, set hidden style, then exit from battery options menu, then enter it again.
    Before the commit it was setting the text style back.
    
    Change-Id: Ic56196870427d11895e2d9e79413d0fcd673b7d3
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -324,6 +324,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
+        mBatteryRemainingIcon.setIsQsHeader(true);
         mBatteryRemainingIcon.setPercentShowMode(getBatteryPercentMode());
         mBatteryRemainingIcon.setOnClickListener(this);
         mRingerModeTextView.setSelected(true);
@@ -565,10 +566,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         boolean showBatteryPercent = Settings.System
                 .getIntForUser(getContext().getContentResolver(),
                 QS_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
-        int batteryMode = showBatteryPercent ?
+        return showBatteryPercent ?
                BatteryMeterView.MODE_ON : BatteryMeterView.MODE_ESTIMATE;
-
-        return batteryMode;
     }
 
     public void setBatteryPercentMode() {
