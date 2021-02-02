9367f7288ae70213d59ce6c749e5d9ad041b082a
TikkiTikki <radstylemusic@gmail.com>
2021-02-02 01:19:07 -0300

    base: Show and edit Ambient Icons on Lockscreen [1/2]
    * set 80dp as default
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5309,6 +5309,24 @@ public final class Settings {
         public static final String FOD_COLOR = "fod_color";
 
         /**
+         * Show ambient icons on lockscreen
+         * @hide
+         */
+        public static final String AMBIENT_ICONS_LOCKSCREEN = "ambient_icons_lockscreen";
+
+        /**
+         * Change size of ambient icons on lockscreen
+         * @hide
+         */
+        public static final String AMBIENT_ICONS_SIZE = "ambient_icons_size";
+
+        /**
+         * Change color of ambient icons on lockscreen
+         * @hide
+         */
+        public static final String AMBIENT_ICONS_COLOR = "ambient_icons_color";
+
+        /**
          * Screenshot image compression
          * @hide
          */
