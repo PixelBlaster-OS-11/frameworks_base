crdroidandroid/android_frameworks_base

base: allow device to override *caugh*caugh* prop
Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65

core/java/android/os/Build.java
@@ -258,6 +258,13 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible omni security patch level.
+         * @hide
+         */
+        public static final String SECURITY_PATCH_LINEAGE = SystemProperties.get(
+                "ro.lineage.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -154,9 +154,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchLineage = Build.VERSION.SECURITY_PATCH_LINEAGE;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchLineage)){
+                    patch = patchLineage;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");

PixelBlaster-OS-11/frameworks_base

(cherry picked from commit 1382f28e52eb33e5da9f45bc97f854671f1efe34)

core/java/android/os/Build.java
@@ -280,6 +280,13 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible Hycon security patch level.
+         * @hide
+         */
+        public static final String SECURITY_PATCH_HYCON = SystemProperties.get(
+                "ro.hycon.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -154,9 +154,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchHycon = Build.VERSION.SECURITY_PATCH_HYCON;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchHycon)){
+                    patch = patchHycon;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");