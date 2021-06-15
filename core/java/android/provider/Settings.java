cd8ee68cae81eb81991351c3e6501e7dd58c8d72
Maitreya29 <maitreyapatni30@gmail.com>
2021-06-15 20:19:27 +0530

    Base: Wallpaper based accent colors [1/2]
    Co-authored-by: tanishqmanuja <tanishqmanuja@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5716,6 +5716,13 @@ public final class Settings {
         };
 
         /**
+         * Wallpaper based accents
+         * @hide
+         */
+        public static final String USE_WALL_ACCENT = "use_wall_accent";
+        public static final String AUTO_ACCENT_TYPE = "auto_accent_type";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -5872,6 +5879,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_FORMAT);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_POSITION);
             PRIVATE_SETTINGS.add(ACCENT_COLOR);
+            PRIVATE_SETTINGS.add(USE_WALL_ACCENT);
+            PRIVATE_SETTINGS.add(AUTO_ACCENT_TYPE);
         }
 
         /**
