fcd92e767eddfdc64701dcd8dcb9e215bb2fb8f9
darknius09 <darkniuslightness@gmail.com>
2023-05-28 07:47:32 +0000

    base: Custom Statusbar Signal & Wi-Fi icons

--- core/java/com/android/internal/util/custom/ThemesUtils.java
+++ core/java/com/android/internal/util/custom/ThemesUtils.java
@@ -80,4 +80,28 @@ public class ThemesUtils {
             }
         }
     }
+
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+        "org.blissroms.systemui.signalbar_a",
+        "org.blissroms.systemui.signalbar_b",
+        "org.blissroms.systemui.signalbar_c",
+        "org.blissroms.systemui.signalbar_d",
+        "org.blissroms.systemui.signalbar_e",
+        "org.blissroms.systemui.signalbar_f",
+        "org.blissroms.systemui.signalbar_g",
+        "org.blissroms.systemui.signalbar_h",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+        "org.blissroms.systemui.wifibar_a",
+        "org.blissroms.systemui.wifibar_b",
+        "org.blissroms.systemui.wifibar_c",
+        "org.blissroms.systemui.wifibar_d",
+        "org.blissroms.systemui.wifibar_e",
+        "org.blissroms.systemui.wifibar_f",
+        "org.blissroms.systemui.wifibar_g",
+        "org.blissroms.systemui.wifibar_h",
+    };
 }
